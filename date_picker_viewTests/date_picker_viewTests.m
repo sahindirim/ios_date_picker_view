37825061ddf3afd5ae7216f0579e1ec91a4a1df2
Şahin Dirim <sahindirim@Sahin-MacBook-Pro.local>
2014-10-20 14:25:11 +0300

    Initial Commit

---
+++ date_picker_viewTests/date_picker_viewTests.m
@@ -0,0 +1,40 @@
+//
+//  date_picker_viewTests.m
+//  date_picker_viewTests
+//
+//  Created by Şahin Dirim on 20.10.2014.
+//  Copyright (c) 2014 Şahin Dirim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface date_picker_viewTests : XCTestCase
+
+@end
+
+@implementation date_picker_viewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
